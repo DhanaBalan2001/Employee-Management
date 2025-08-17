d03779bfbf19664e72a0c52fe3e0cc3967aa0fc1
DhanaBalan2001 <dhanabalank22@gmail.com>
2025-08-17 20:40:03 +0530

    Third Commit

--- server/server.js
+++ server/server.js
@@ -52,12 +52,14 @@ app.use('/api/automation', automationRoutes);
 app.use('/api/notifications', notificationRoutes);
 
 // Serve static files from React build
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
