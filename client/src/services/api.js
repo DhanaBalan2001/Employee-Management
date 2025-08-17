c1a390b854e480cf3986804872c937c411866f63
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-08-17 20:47:41 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://employee-management-bj7q.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
